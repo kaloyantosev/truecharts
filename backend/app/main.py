525e65b7adf58d508f461192e0ca8cc3143d7461
kaloyantosev <kaloyan.tosev@gmail.com>
2026-07-07 13:01:06 +0300

    Add files via upload

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3001"
     ],
+    allow_origin_regex="https://.*\\.vercel\\.app",
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
